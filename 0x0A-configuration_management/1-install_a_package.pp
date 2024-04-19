d7de4dc3da4b13c2c56cdd3aab048068a4807b34
RamzyAR7 <rameya683@gmail.com>
2024-04-20 00:16:30 +0200

    add v2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,10 @@
-# 1-install_werzeug.pp
-# Install the werzeug package using pip3
-package { 'werzeug':
-  ensure   => 'latest',
+#!/usr/bin/pup
+# install a specific version of Flask (2.1.0)
+package { 'werkzeug':
+  ensure   => '2.0.2',
   provider => 'pip3',
 }
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
