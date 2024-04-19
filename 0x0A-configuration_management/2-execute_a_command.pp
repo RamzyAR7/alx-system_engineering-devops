3a191b9eb1ba49fea4c9f0207ada70ebe466ee33
RamzyAR7 <rameya683@gmail.com>
2024-04-20 00:21:34 +0200

    add v2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,9 +1,9 @@
 #!/usr/bin/pup
 # 2-execute_a_command.pp
+# kill_process.pp
 
-# Use exec resource to kill the process named 'killmenow' using pkill
-exec { 'killmenow':
-  command     => 'pkill killmenow',
-  path        => '/bin:/usr/bin:/usr/local/bin', # Ensure pkill is in the path
-  refreshonly => true, # Execute only when notified by another resource
+exec { 'Kill killmenow process':
+  command     => 'pkill -f killmenow',
+  path        => ['/bin', '/usr/bin'],
+  refreshonly => true,
 }
