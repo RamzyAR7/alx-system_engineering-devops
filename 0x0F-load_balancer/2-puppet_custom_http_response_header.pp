62d6eb3919267b5c2eeca0b43f1d3a9b2c716270
RamzyAR7 <rameya683@gmail.com>
2024-04-30 20:10:35 +0300

    add v2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,10 @@
+#!/usr/bin/pup
+# Install Nginx web server (w/ Puppet)
+
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
+}
