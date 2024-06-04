06e20510677916d2d11474e648d2cca29d9595b1
RamzyAR7 <rameya683@gmail.com>
2024-06-05 01:34:08 +0300

    ramzy

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,10 +10,10 @@ def top_ten(subreddit):
     # URL for the subreddit's hot posts
     url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
     headers = {'User-Agent': 'Mozilla/5.0'}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        
+
         if response.status_code == 200:
             data = response.json()
             posts = data['data']['children']
