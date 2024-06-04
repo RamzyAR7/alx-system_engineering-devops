d95d3afbe7c959cd3b2e0e314c2637867a544f82
RamzyAR7 <rameya683@gmail.com>
2024-06-05 01:23:05 +0300

    ramzy

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,19 +5,14 @@ If an invalid subreddit is given, the function should return 0.
 """
 import requests
 
+
+
 def number_of_subscribers(subreddit):
-    """URL for the subreddit's about.json endpoint"""
+    """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        data = response.json()
-        return data.get("data").get("subscribers")
-    else:
-        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
